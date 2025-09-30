99202b5e4959b526a714d576cd270bcbde80b011
jagruthip2007 <jagruthipulumati@Jagruthis-MacBook-Pro.local>
2025-09-30 12:40:18 +0530

    changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,26 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://simora-caption-generator.vercel.app/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 const uploadRoutes = require("./routes/upload");
 
 const PORT = process.env.PORT || 3001;
@@ -43,7 +63,9 @@ app.use("*", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`🚀 Video Caption Backend server is running on port ${PORT}`);
-  console.log(`📡 Health check: http://localhost:${PORT}/health`);
+  console.log(
+    `📡 Health check: https://simora-caption-generator.onrender.com/health`
+  );
   if (process.env.HINGLISH_PYTHON) {
     console.log(`🚀🚀🚀 Hinglish Python: ${process.env.HINGLISH_PYTHON}`);
   } else {
